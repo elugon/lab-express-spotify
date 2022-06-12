bff8d6bcec0ace186dc5258f6c24e725a5f0f2d7
Elu Gonzalez <elugon10@gmail.com>
2022-06-12 14:02:04 +0200

    lab done

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
 
+const SpotifyWebApi = require('spotify-web-api-node');
+
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -13,6 +15,56 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 // Our routes go here:
+
+app.get("/",(req,res)=>{
+    res.render("home")
+});
+
+app.get('/artist-search', (req, res) => {
+  spotifyApi
+      .searchArtists(req.query.artist)
+      .then(data => {
+          console.log('The received data from the API: ', data.body);
+          // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+          res.render('artist-search-results', { artist: data.body.artists.items })
+      })
+      .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+
+app.get('/albums/:id', (req, res) => {
+  spotifyApi
+      .getArtistAlbums(req.params.id)
+      .then((data) => {
+          res.render('albums', { albums: data.body.items })
+          console.log(data.body);
+      })
+      .catch((err) => console.error("Error to get album from API", err));
+})
+
+
+
+app.get("/tracks/:id", (req, res) => {
+  spotifyApi.getAlbumTracks(req.params.id)
+      .then((data) => {
+          res.render('tracks', { tracks: data.body.items })
+          console.log('Track info', data.body)
+      })
+      .catch(err => console.log('Something went getting the tracks!', err));
+})
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
